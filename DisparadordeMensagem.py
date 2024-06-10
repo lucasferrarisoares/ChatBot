af428022a810c82beb5064115d03351073bc02da
Lukinhaa <lucasferrarisoares@Gmail.com>
2024-06-10 10:43:04 -0300

    Update DisparadordeMensagem.py
    Adicionada a opção de envio por zona de interesse.
    
    O usuário pode escolher entre enviar apenas para pessoas que selecionaram o curso de envio, ou para todos que selecionaram algum curso na área do curso de envio.

--- DisparadordeMensagem.py
+++ DisparadordeMensagem.py
@@ -11,6 +11,7 @@ import pandas as pd
 import webbrowser
 import threading
 
+
 # Criar uma classe para a GUI
 class CourseOfferGUI:
     def __init__(self, root):
@@ -57,6 +58,11 @@ class CourseOfferGUI:
         self.duration_entry = ttk.Entry(root, width=30)
         self.duration_entry.pack()
 
+        self.group_label = ttk.Label(root, text="Deseja realizar o envio por grupo?(SIM/NÃO):")
+        self.group_label.pack()
+        self.group_entry = ttk.Entry(root, width=30)
+        self.group_entry.pack()
+
         self.minrange_label = ttk.Label(root, text="De qual linha devo começar:")
         self.minrange_label.pack()
         self.minrange_entry = ttk.Entry(root, width=30)
@@ -77,6 +83,7 @@ class CourseOfferGUI:
 
         self.running = False
 
+
     def interromper_codigo(self):
         self.running = False
 
@@ -89,6 +96,29 @@ class CourseOfferGUI:
         t = threading.Thread(target=self.send_messages)
         t.start()
 
+    def encontra_categoria(self, curso_de_envio: str) -> list:
+        #DEFINE AS CATEGORIAS
+        Dev=["INTRODUÇÃO A LÓGICA DE PROGRAMAÇÃO", "INTRODUÇÃO A LÓGICA DE PROGRAMAÇÃO COM ALGORÍTIMOS", "Montagem e Manutenção de Computadores e Redes", "Programação, Redes e programação", 
+             "SQL Linguagem de programação", "Inteligência artificial", "Cloud", "DBA", "Big Data", "Desenvolvimento web", "Programação Avançada", "Introdução a Programação", "Lógica de Programação"]
+
+        Marketing=["REDES SOCIAIS: UM GUIA PRÁTICO PARA ALAVANCAR SUAS VENDAS", "CRIAÇÃO DE MÍDIAS PARA REDES SOCIAIS", "INSTAGRAM PARA NEGÓCIOS", "Marketing Digital"]      
+
+        Design=["CRIAÇÃO DE MÍDIAS PARA REDES SOCIAIS", "SKETCHUP", "Design", "SKETCHUP - DESENHOS DE AMBIENTES E OBJETOS EM 3D - NOITE", "SketchUp 3D"]
+
+        Pacote_Office=["EXCEL - INTERMEDIÁRIO", "EXCEL - AVANÇADO", "PACOTE OFFICE", "Edição de Texto", "Planilha Eletrônica"]
+
+        Basicos=["INFORMÁTICA BÁSICA (TERCEIRA IDADE)", "INTRODUÇÃO A INFORMÁTICA (WINDOWS; WORD, EXCEL; POWERPOINT E INTERNET)", "Informática básica", "Informática básica (terceira idade)", 
+                 "Informática básica a noite", "Informática básica (jovens)", "INFORMÁTICA BÁSICA (TERCEIRA IDADE) - MANHÃ", "Informatica basica a noite", "Introdução de Informática - TARDE - 22/01 à 21/02/24", 
+                 "Introdução de Informática"]
+        lista_categoria = [Dev, Marketing, Design, Pacote_Office, Basicos]
+
+        Correto = None
+        while Correto == None:
+            for Categorias in lista_categoria:
+                if curso_de_envio in Categorias:
+                    Correto = Categorias
+        return Correto
+
 
     def send_messages(self):
         # Obter entrada do usuário
@@ -100,6 +130,7 @@ class CourseOfferGUI:
         linhamin = int(self.minrange_entry.get())
         linhamax = int(self.maxrange_entry.get())
         idademin = int(self.minage_entry.get())
+        por_grupo = self.group_entry.get()
     
 
         # Ler o arquivo Excel
@@ -119,19 +150,35 @@ class CourseOfferGUI:
             cursos = alunos.loc[x, "Dentre as opções qual curso gostaria de fazer?"]
             lista_cursos = cursos.split(sep=', ')
 
-            #olha cada item da lista criada, verifica se o curso de envio está dentro da lista, caso esteja, a planilha pega o nome e telefone do aluno, e faz o envio da mensagem.
-            for curso in lista_cursos:
-                if curso.upper() == curso_de_envio.upper():
-                    # Ler planilha e guardar informações nome e telefone
-                    nome = alunos.loc[x, 'Nome Completo']
-                    telefone = alunos.loc[x, 'Whatsapp com DDD (somente números - sem espaço)']
+            if por_grupo == "SIM":
+                categoria = self.encontra_categoria( curso_de_envio)
+                for curso in lista_cursos:
+                    if curso in categoria:
+                        # Ler planilha e guardar informações nome e telefone
+                        nome = alunos.loc[x, 'Nome Completo']
+                        telefone = alunos.loc[x, 'Whatsapp com DDD (somente números - sem espaço)']
 
-                    mensagem = "Olá {0}. Nós somos da AMTECH - Agência Maringá de Tecnologia e Inovação. estamos entrando em contato pois você respondeu um formulário de interesse em cursos na área de tecnologia.\nNós iremos iniciar em parceria com o {1}, o curso {2}.\nTodos podem participar desde que sejam maior de {3} anos e tenham a escolaridade mínima 5º ano do Ensino Fundamental.\nO curso tem duração do dia {4} e será no período {5} das {6}\nAqueles que tiverem interesse, favor respondam essa mensagem, que iremos enviar o formulário para preenchimento dos dados\nATENÇÃO!\nPois as vagas são LIMITADAS!!".format(nome, parceiro, curso_de_envio, idademin, data_de_duracao, periodo, horario_do_curso)
+                        mensagem = "Olá {0}. Nós somos da AMTECH - Agência Maringá de Tecnologia e Inovação. estamos entrando em contato pois você respondeu um formulário de interesse em cursos na área de tecnologia.\nNós iremos iniciar em parceria com o {1}, o curso {2}.\nTodos podem participar desde que sejam maior de {3} anos e tenham a escolaridade mínima 5º ano do Ensino Fundamental.\nO curso tem duração do dia {4} e será no período {5} das {6}\nAqueles que tiverem interesse, favor respondam essa mensagem, que iremos enviar o formulário para preenchimento dos dados\nATENÇÃO!\nPois as vagas são LIMITADAS!!".format(nome, parceiro, curso_de_envio, idademin, data_de_duracao, periodo, horario_do_curso)
 
-                    # Criar links personalizados do whatsapp e enviar mensagens para cada cliente
-                    link_mensagem_whatsapp = f'https://api.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
-                    webbrowser.open(link_mensagem_whatsapp)
-                    sleep(5)
+                        # Criar links personalizados do whatsapp e enviar mensagens para cada cliente
+                        link_mensagem_whatsapp = f'https://api.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+                        webbrowser.open(link_mensagem_whatsapp)
+                        sleep(5)
+                        break
+            else:
+                #olha cada item da lista criada, verifica se o curso de envio está dentro da lista, caso esteja, a planilha pega o nome e telefone do aluno, e faz o envio da mensagem.
+                for curso in lista_cursos:
+                    if curso.upper() == curso_de_envio.upper():
+                        # Ler planilha e guardar informações nome e telefone
+                        nome = alunos.loc[x, 'Nome Completo']
+                        telefone = alunos.loc[x, 'Whatsapp com DDD (somente números - sem espaço)']
+
+                        mensagem = "Olá {0}. Nós somos da AMTECH - Agência Maringá de Tecnologia e Inovação. estamos entrando em contato pois você respondeu um formulário de interesse em cursos na área de tecnologia.\nNós iremos iniciar em parceria com o {1}, o curso {2}.\nTodos podem participar desde que sejam maior de {3} anos e tenham a escolaridade mínima 5º ano do Ensino Fundamental.\nO curso tem duração do dia {4} e será no período {5} das {6}\nAqueles que tiverem interesse, favor respondam essa mensagem, que iremos enviar o formulário para preenchimento dos dados\nATENÇÃO!\nPois as vagas são LIMITADAS!!".format(nome, parceiro, curso_de_envio, idademin, data_de_duracao, periodo, horario_do_curso)
+
+                        # Criar links personalizados do whatsapp e enviar mensagens para cada cliente
+                        link_mensagem_whatsapp = f'https://api.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+                        webbrowser.open(link_mensagem_whatsapp)
+                        sleep(5)
 
         print("Todas as linhas foram lidas!")
         self.running = False
